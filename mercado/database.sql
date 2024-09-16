06d25d8d87f4a505c6b145f320a745c975a3368e
Karla C. Ferreira <karlinharural@gmail.com>
2024-09-16 15:36:50 -0300

    Update database.sql

--- mercado/database.sql
+++ mercado/database.sql
@@ -1,24 +1,48 @@
-CREATE TABLE Endereco (
+CREATE TABLE Endereco ( --nao tem essa table mas achei melhor ter
 	cod_endereco SERIAL PRIMARY KEY,
 	endereco varchar(30),
-	cep varchar(15),
+	cidade varchar(15),
+	cep varchar(8),
 	uf varchar(2),
-	cgc int(20),
+	cgc numeric(20),
 	inscr_estadual varchar(20)
 );
 
 CREATE TABLE Cliente (
-	cod_cliente int(5) PRIMARY KEY,
-	nome varchar(20),
-	cod_endereco int,
+	cod_cliente numeric(5) PRIMARY KEY,
+	nome_cliente varchar(20) NOT NULL,
+	cod_endereco integer,
 	FOREIGN KEY (cod_endereco) REFERENCES Endereco(cod_endereco)
 );
 
+CREATE TABLE Vendedor (
+	cod_vendedor numeric(3) PRIMARY KEY,
+	nome_vendedor varchar(20) NOT NULL,
+	salario_fixo money,
+	faixa_comissao varchar(1)
+);
+
 CREATE TABLE Pedido (
-	num_pedido int(5) PRIMARY KEY,
-	prazo_entrega int(3),
-	cod_cliente int(5),
-	cod_vendedor int(5),
+	cod_pedido numeric(5) PRIMARY KEY,
+	prazo_entrega numeric(3),
+	cod_cliente numeric(5),
+	cod_vendedor numeric(3),
 	FOREIGN KEY (cod_cliente) REFERENCES Cliente(cod_cliente),
 	FOREIGN KEY (cod_vendedor) REFERENCES Vendedor(cod_vendedor)
+);
+
+CREATE TABLE Produto (
+	cod_produto numeric(7) PRIMARY KEY,
+	unidade varchar(3),
+	descricao varchar(30),
+	valor_unitario money
+);
+
+CREATE TABLE Item_Pedido (
+	cod_pedido numeric(5),
+	cod_produto numeric(7),
+	quantidade numeric(9),
+	PRIMARY KEY (cod_pedido, cod_produto),
+	FOREIGN KEY (cod_pedido) REFERENCES Pedido(cod_pedido),
+	FOREIGN KEY (cod_produto) REFERENCES Produto(cod_produto)
 );
