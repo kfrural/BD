4189f4adb468945cc247faec2c6917d6a192bc5d
Karla C. Ferreira <karlinharural@gmail.com>
2024-09-30 15:02:45 -0300

    feito_completo_database.sql

--- academico/database.sql
+++ academico/database.sql
@@ -84,3 +84,72 @@ CREATE TABLE Horario (
 	FOREIGN KEY (num_sala) REFERENCES Sala (num_sala)
 );
 
+INSERT INTO Dpto (cod_dpto, nome_dpto) VALUES (1, 'Departamento de Ciências Exatas');
+INSERT INTO Dpto (cod_dpto, nome_dpto) VALUES (2, 'Departamento de Humanas');
+INSERT INTO Dpto (cod_dpto, nome_dpto) VALUES (3, 'Departamento de Engenharia');
+
+INSERT INTO Titulacao (cod_tit, nome_tit) VALUES (1, 'Mestre');
+INSERT INTO Titulacao (cod_tit, nome_tit) VALUES (2, 'Doutor');
+INSERT INTO Titulacao (cod_tit, nome_tit) VALUES (3, 'Especialista');
+
+INSERT INTO Professor (cod_prof, cod_dpto, cod_tit, nome_prof) VALUES (1, 1, 1, 'Prof. João');
+INSERT INTO Professor (cod_prof, cod_dpto, cod_tit, nome_prof) VALUES (2, 2, 2, 'Prof. Tavares');
+INSERT INTO Professor (cod_prof, cod_dpto, cod_tit, nome_prof) VALUES (3, 3, 3, 'Prof. Maria');
+
+INSERT INTO Disciplina (cod_dpto, num_disc, nome_disc, creditos_disc) VALUES (1, 101, 'Cálculo I', 4);
+INSERT INTO Disciplina (cod_dpto, num_disc, nome_disc, creditos_disc) VALUES (2, 202, 'Filosofia', 3);
+INSERT INTO Disciplina (cod_dpto, num_disc, nome_disc, creditos_disc) VALUES (3, 303, 'Engenharia de Software', 5);
+
+INSERT INTO Pre_req (cod_dpto, num_disc, num_disc_req) VALUES (1, 101, 100);
+INSERT INTO Pre_req (cod_dpto, num_disc, num_disc_req) VALUES (2, 202, 201);
+INSERT INTO Pre_req (cod_dpto, num_disc, num_disc_req) VALUES (3, 303, 302);
+
+INSERT INTO Turma (cod_dpto, num_disc, anos_sem, sigla_tur) VALUES (1, 101, 2024, 'A');
+INSERT INTO Turma (cod_dpto, num_disc, anos_sem, sigla_tur) VALUES (2, 202, 2024, 'B');
+INSERT INTO Turma (cod_dpto, num_disc, anos_sem, sigla_tur) VALUES (3, 303, 2024, 'C');
+
+INSERT INTO Prof_Turma (cod_dpto, num_disc, anos_sem, sigla_tur, cod_prof) VALUES (1, 101, 2024, 'A', 1);
+INSERT INTO Prof_Turma (cod_dpto, num_disc, anos_sem, sigla_tur, cod_prof) VALUES (2, 202, 2024, 'B', 2);
+INSERT INTO Prof_Turma (cod_dpto, num_disc, anos_sem, sigla_tur, cod_prof) VALUES (3, 303, 2024, 'C', 3);
+
+INSERT INTO Predio (cod_predio, descricao_predio) VALUES (1, 'Prédio Principal');
+INSERT INTO Predio (cod_predio, descricao_predio) VALUES (2, 'Prédio de Engenharia');
+INSERT INTO Predio (cod_predio, descricao_predio) VALUES (3, 'Prédio de Humanas');
+
+INSERT INTO Sala (cod_predio, num_sala, descricao_sala, capacidade) VALUES (1, 101, 'Sala 101', 40);
+INSERT INTO Sala (cod_predio, num_sala, descricao_sala, capacidade) VALUES (2, 202, 'Sala 202', 30);
+INSERT INTO Sala (cod_predio, num_sala, descricao_sala, capacidade) VALUES (3, 303, 'Sala 303', 50);
+
+INSERT INTO Horario (cod_dpto, num_disc, anos_sem, sigla_tur, dia_sem, hora_inicio, cod_predio, num_sala, num_horas) 
+VALUES (1, 101, 2024, 'A', 2, '08:00:00', 1, 101, 2);
+
+INSERT INTO Horario (cod_dpto, num_disc, anos_sem, sigla_tur, dia_sem, hora_inicio, cod_predio, num_sala, num_horas) 
+VALUES (2, 202, 2024, 'B', 3, '10:00:00', 2, 202, 3);
+
+INSERT INTO Horario (cod_dpto, num_disc, anos_sem, sigla_tur, dia_sem, hora_inicio, cod_predio, num_sala, num_horas) 
+VALUES (3, 303, 2024, 'C', 4, '14:00:00', 3, 303, 2);
+
+
+--exercicio 1
+SELECT p.descricao_predio, COUNT(s.num_sala) AS qtd_salas
+FROM Predio p LEFT JOIN Sala s ON p.cod_predio = s.cod_predio
+GROUP BY p.descricao_predio
+ORDER BY p.descricao_predio;
+
+--exerccio 2
+SELECT p.descricao_predio, COUNT(s.num_sala) AS qtd_sala
+FROM Predio p LEFT JOIN Sala s ON p.cod_predio = s.cod_predio
+GROUP BY p.descricao_predio
+HAVING COUNT (s.num_sala) > 3;
+
+--exercicio 3
+SELECT d.nome_disc, t.sigla_tur FROM Professor p
+JOIN Prof_Turma pt ON p.cod_prof = pt.cod_prof
+JOIN Turma t ON pt.cod_dpto = t.cod_dpto AND pt.num_disc = t.num_disc AND pt.anos_sem = t.anos_sem AND pt.sigla_tur = t.sigla_tur
+JOIN Disciplina d ON t.cod_dpto = d.cod_dpto AND t.num_disc = d.num_disc
+WHERE p.nome_prof = 'Tavares';
+
+--exercicio 4
+SELECT d.nome_disc, t.sigla_tur
+FROM Disciplina d
+LEFT JOIN Turma t ON d.cod_dpto = t.cod_dpto AND d.num_disc = t.num_disc;
